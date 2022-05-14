658764de2502d2b61e800ba93b34875990cf7196
lichtgestalter <uli.scheuss@gmail.com>
2022-05-14 19:26:04 +0200

    nochmal

--- ssls.py
+++ ssls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #branch
 # SSLS - Star System Lightcurve Simulator
+#xxxxxxxxxxxxxxxxx
 # The SSLS calculates the movements and eclipses of celestial bodies and produces a video of this.<br>
 # Specify mass, radius and other properties of some stars and planets in a configuration file. Then run "ssls.py <Configfilename>" to produce the video.<br>
 # The video shows simultanously a view of the star system from top and from the side and the lightcurve of the system's total luminosity over time.<br>
